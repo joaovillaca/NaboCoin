73472df1ed6fbddb23051f8e70f0ad35e4d0ab76
Joao Villaca <jvnvillaca@gmail.com>
2021-05-16 18:25:56 -0300

    Auto stash before merge of "main" and "origin/main"

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+<<<<<<< Updated upstream
 app = Flask(__name__, template_folder='templates')
+=======
+app = Flask(__name__, template_folder="templates")
+>>>>>>> Stashed changes
 
 import application.main
--- application/views/views.py
+++ application/views/views.py
@@ -1,11 +1,26 @@
+<<<<<<< Updated upstream:application/views/views.py
 from application import app
 import json
+=======
+from flask import app, render_template
+from flask.wrappers import JSONMixin
+from application.Blockchain import Blockchain
+>>>>>>> Stashed changes:application/routes.py
 
-
-@app.route('/chain', methods=['GET'])
-def get_chain():
+@app.route("/", methods=['GET'])
+def homepage():
+    
     chain_data = []
     for block in blockchain.chain:
         chain_data.append(block.__dict__)
+<<<<<<< Updated upstream:application/views/views.py
     return json.dumps({"length": len(chain_data),
                        "chain": chain_data})
+=======
+    
+    JSONMixin.dumps({"length": len(chain_data),
+                       "chain": chain_data})
+
+    return render_template('homepage.html') 
+    
+>>>>>>> Stashed changes:application/routes.py
