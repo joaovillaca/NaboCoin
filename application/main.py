0282ade9b06ab210f45df6f6328fe6f43a499ce8
Joao Villaca <jvnvillaca@gmail.com>
2021-05-19 19:58:47 -0300

    added icon, session, user page

--- application/main.py
+++ application/main.py
@@ -1,11 +1,17 @@
-from flask import render_template
-from flask.helpers import flash
-from flask.wrappers import Request
-from werkzeug.wrappers import UserAgentMixin
+from flask.wrappers import JSONMixin
 from application import app
 from application.blockchain import blockchain
+from flask import render_template, redirect, url_for, request, session, flash
 from application.forms import RegistrationForm
+from werkzeug.wrappers import UserAgentMixin
 import json
+
+@app.route('/')
+def home():
+    message = "Últimos blocos da Nabocoin:"
+    action = get_chain()
+
+    return render_template('index.html', action=action, message=message)
 
 @app.route('/chain', methods=['GET'])
 def get_chain():
@@ -15,11 +21,6 @@ def get_chain():
     return json.dumps({"length": len(chain_data),
                        "chain": chain_data})
 
-@app.route('/')
-def home():
-    message = "Últimos blocos da Nabocoin:"
-    action = get_chain()
-    return render_template('index.html', action=action, message=message)
 
 @app.route('/buy')
 def comprar():
@@ -27,24 +28,42 @@ def comprar():
     action ="Comprar"
     return render_template('index.html', action=action, message=message)
 
-@app.route('/login')
-def login():
-    action ="Login"
-    return render_template('index.html', action=action)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     action ="Cadastro"
+
     form = RegistrationForm()
-    if Request.method == 'POST' and form.validate():
-        user = UserAgentMixin(form.username.data, form.email.data,
-                    form.password.data)
+    if request.method == 'POST' and form.validate():
+        user = UserAgentMixin(form.username.data, form.email.data, form.password.data)
         flash('Thanks for registering' + ' ' + user)
         return redirect(url_for('login'))
+
     return render_template('forms.html', action=action, form=form)
 
+
+@app.route('/login')
+def login():
+    action ="Login"
+
+    if request.method == "POST":
+        user = request.form["login"]
+        session["user"] = user
+        return redirect(url_for("user"))
+
+    return render_template('index.html', action=action)
+
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.hmtl")
+    else:
+        return redirect(url_for("login"), user=user)
+
 # secret key generated using python3 CLI interpreter
-# used to protect the application against cross-site scripting (also knows as XSS) and cookie modificcation
+# used to protect the application against cross-site scripting and cookie modificcation
 # >> import secrets
 # >> secrets.token_hex(32)
 app.config['SECRET_KEY'] = 'e41ea80c121d9f8778579e53a29e31865739d4cf749b0cb1f04e2c629743a80f'
--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 #!flask/bin/python
 from application.main import app
 
-
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
