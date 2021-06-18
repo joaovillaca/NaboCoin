829edacc30f2391c4b7b0500d6396393c125980f
Joao Villaca <jvnvillaca@gmail.com>
2021-06-18 04:50:19 -0300

    Requirements/gitignore update

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+import os
 from setuptools import setup
 
 setup(
-    name='application',
-    packages=['application'],
-    include_package_data=True,
-    install_requires=[
-        'flask',
+    name = "Nabocoin",
+    version = "0.0.1",
+    author = "https://github.com/zerodois-bcc/",
+    description = ("O blockchain-as-a-service mais simples que existe, implementado em Python e Flask. Projeto para as matérias SSC0158 e SSC0147."),
+    license = "BSD",
+    url = "https://github.com/zerodois-bcc/NaboCoin",
+    requires = [
+        "flask",
+        "flask_sqlalchemy",
+        "werkzeug"
     ],
+    classifiers=[
+        "Development Status :: Initial Mockup",
+        "License :: BSD License",
+    ]
 )
